96cd91caae8eeccfd67ddad7b651ad67718b72fb
adolfo <adolfo.tamayo@ucsp.edu.pe>
2015-10-06 08:54:05 -0500

    Problem 3, js subprograms done

--- Chap5PE/subprograms.js
+++ Chap5PE/subprograms.js
@@ -3,13 +3,33 @@ function parent () {
     var parent_var = 0;
     function level1(){
         var l1_var = 1;
-        console.log("Parent var: {0}".format(parent_var))
+        console.log("parent_var from L1:" + parent_var);
         function level2(){
             var l2_var = 2;
+            console.log("parent_var from L2: " + parent_var);
+            console.log("l1_var from L2: " + l1_var);
             function level3(){
-                var l3_var = 3;
+                console.log("parent_var from L3:" + parent_var);
+                console.log("l1_var from L3:" + l1_var); 
+                console.log("l2_var from L3:" + l2_var);
             }
+            level3();
         }
-
+        level2();
     }
+    level1();
 }
+parent();
+//Output:
+/*
+
+parent_var from L1:0
+parent_var from L2: 0
+l1_var from L2: 1
+parent_var from L3:0
+l1_var from L3:1
+l2_var from L3:2
+
+
+
+*/
