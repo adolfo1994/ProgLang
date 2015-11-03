39c4af82348281668455a08fe10b2788c30cb22a
Adolfo Tamayo <adolfo.tamayo@ucsp.edu.pe>
2015-11-03 09:53:37 -0500

    exercise 8 Perl hashes done

---
+++ Chap6PE/ex8.pl
@@ -0,0 +1,42 @@
+use warnings;
+
+use Time::HiRes qw( time );
+use List::Util qw(first);
+
+srand( time() ^ ($$ + ($$ << 15)) );
+my @alphabet = qw ( a b c d e f g h i j k l m n o p q r s t u v w x y z );
+my @hash;  
+my @list;
+my $name;
+sub hash {
+    for (my $i = 0; $i < 10000; $i++) {
+        my $age = rand(90);
+        my $name .= $alphabet[int(rand(26))] for 1 .. 3;
+        if(! $hash[$name]){
+            $hash[$name] = $age;
+        }
+    }   
+}
+sub nohash{
+    for (my $i = 0; $i < 10000; $i++) {
+        my $age = rand(90);
+        my $name .= $alphabet[int(rand(26))] for 1 .. 3;
+        push(@list, $age);
+    }   
+}
+
+my $start_hash = time();
+hash();
+my $end_hash = time();
+
+printf("With hash it took: %.2f seconds\n", $end_hash - $start_hash);
+
+
+my $start_no_hash = time();
+nohash();
+my $end_no_hash = time();
+printf("Without hash it took: %.2f seconds\n", $end_no_hash - $start_no_hash);
+
+# El programa con hash toma mas tiempo que el que no lo utiliza
+# With hash it took: 1.40 seconds
+# Without hash it took: 0.01 seconds
